d9678c5a2e8cf61a5fae115571d6c3568f2379c8
Caleb Henning <chenning@wesleyan.edu>
2024-11-07 15:39:52 -0500

    adding documentation for starcatalog dataset

---
+++ R/starcatalog.R
@@ -0,0 +1,19 @@
+#' @title Catalog of 8774 stars and some information on them
+#'
+#' @description
+#' Here is a paragraph describing the data.
+#'
+#' @export
+#'
+#' @source where i got the data from
+#' @format A data frame with 8774 rows and 8 variables:
+#' \describe{
+#'   \item{\code{name}}{character. name of the star.}
+#'   \item{\code{alt_name}}{character. alternate name of the star, if applicable.}
+#'   \item{\code{vmag}}{double. v magnitude.}
+#'   \item{\code{multiple}}{character. DESCRIPTION.}
+#'   \item{\code{class}}{character. DESCRIPTION.}
+#'   \item{\code{bv_color}}{double. DESCRIPTION.}
+#'   \item{\code{parallax}}{double. DESCRIPTION.}
+#'   \item{\code{spect_type}}{character. DESCRIPTION.}
+#' }
