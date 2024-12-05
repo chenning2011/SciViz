da790daeb2a45acb987ffea6e88acdbe794faeac
Angelica Crown <acrown@wesleyan.edu>
2024-12-05 00:45:59 -0500

    adding more documentation and updating network fxn

--- R/interact_network_plot.R
+++ R/interact_network_plot.R
@@ -1,5 +1,5 @@
-#'@title Interactive Network Plot
-#'@description Create interactive network plot for a data frame of edge list format. Designed for exploratory data analysis.
+#'@title Network Plot for Interaction or Adjacency Matrix
+#'@description Create interactive network plot for a data frame of edge list format. Designed for exploratory data analysis or visualization of adjacency matrices.
 #'@export
 #'@param data a data frame of edge list format. This means each row should specify a link between 2 nodes. There should be two columns; one for the inital point, and a second target point.
 #'@param initial_pt column NUMBER of initial point variable. Default is 1
@@ -23,6 +23,23 @@
 #'interact_network_plot(network_data_test, initial_pt=1, target_pt = 2)
 #'
 #'interact_network_plot(network_data_test, initial_pt = 3, target_pt = 4)
+
+
+
+
+#need to add if statments for methods
+#error if wrong data submitted for method
+#update documentation for aurguments
+#dont need to add new ones but might--look at what options are for the adjacency fxn
+#add html::tools to adjacency?
+#update rmd arguments and results and examples --mostly EXAMPLES for presentation
+#update documentation for both adjacency and interactive plot
+#need to make sure something will work with default --can the same data frame work for both methods?
+  #add more columns to test data and it might, just have more options for interactive plot
+#interactive plot needs to be edge list format? look back at documentation
+#be specific about what is necessary for adjacency vs interactive and what their different uses are
+
+#add descriptions to datasets
 
 interact_network_plot <- function(data, initial_pt=1, target_pt=2, linkDistance=10, linkColour= "#666", nodeColour= "#69b3a2", opacity= 0.9, title="Network Graph", subtitle="Exploring Interactions and Relationships"){
 
@@ -56,3 +73,29 @@ interact_network_plot <- function(data, initial_pt=1, target_pt=2, linkDistance=
                               ))
 
 }
+
+#library
+library(igraph)
+
+# Create data
+set.seed(10)
+data <- matrix(sample(0:2, 25, replace=TRUE), nrow=5)
+colnames(data) = rownames(data) = LETTERS[1:5]
+
+# build the graph object
+network <- graph_from_adjacency_matrix(data)
+
+# plot it
+plot(network)
+
+# data
+set.seed(1)
+data <- matrix(sample(0:2, 15, replace=TRUE), nrow=3)
+colnames(data) <- letters[1:5]
+rownames(data) <- LETTERS[1:3]
+
+# create the network object
+network <- graph_from_incidence_matrix(data)
+
+# plot it
+plot(network)
